89eaf2205ac464819cb71de26367d69738202307
Mike Johnson <mike@mapleprintservices.com>
2023-04-07 20:51:49 -0400

    data updated route dashboard and layout components created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { useMemo } from "react"; 
+import { themeSettings } from "theme";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout";
+
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <ThemeProvider theme = {theme}>
+          {/* cssBaseline from materialUI acts like a css reset */}
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
