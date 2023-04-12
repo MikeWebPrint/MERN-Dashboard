3b45c4e72dbffebe91df36c095ee673a29b6494c
Mike Johnson <mike@mapleprintservices.com>
2023-04-12 13:49:43 -0400

    init navbar and items

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,159 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate, useNavigation } from 'react-router-dom';
+import profileImage from "assets/profile.jpeg";
+import FlexBetween from './FlexBetween';
 
-const Sidebar = () => {
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme
+} from "@mui/material";
+
+import {
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOutlined,
+  PointOfSaleOutlined,
+  TodayOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PieChartOutlined
+} from "@mui/icons-material";
+
+const navItems =[
+  {
+    text: "Dashboard",
+    icon: <HomeOutlined />
+  },
+  {
+    text: "Client Facing",
+    icon: null
+  },
+  {
+    text: "Products",
+    icon: <ShoppingCartOutlined />
+  },
+  {
+    text: "Customers",
+    icon: <Groups2Outlined />
+  },
+  {
+    text: "Transactions",
+    icon: <ReceiptLongOutlined />
+  },
+  {
+    text: "Geography",
+    icon: <PublicOutlined />
+  },
+  {
+    text: "Sales",
+    icon: null
+  },
+  {
+    text: "Overview",
+    icon: <PointOfSaleOutlined />
+  },
+  {
+    text: "Daily",
+    icon: <TodayOutlined />
+  },
+  {
+    text: "Monthly",
+    icon: <CalendarMonthOutlined />
+  },
+  {
+    text: "Breakdown",
+    icon: <PieChartOutlined />
+  },
+  {
+    text: "Management",
+    icon: null
+  },
+  {
+    text: "Admin",
+    icon: <AdminPanelSettingsOutlined />
+  },
+  {
+    text: "Performance",
+    icon: <TrendingUpOutlined />
+  },
+]
+
+const Sidebar = ({
+  drawerWidth,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  isNonMobile,
+}) => {
+  // will deternmine what page we are currently on
+  const { pathname } = useLocation();
+  const [active, setActive] = useState('');
+  const navigate = useNavigate();
+  // to navigate to other pages
+  const theme = useTheme();
+
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
+  // any time pathname changes, active value is set to current page URL
   return (
-    <div>Sidebar</div>
-  )
-}
+    // going to use material ui persistent drawer
+    <Box component="nav">
+      {isSidebarOpen && (
+        <Drawer 
+        open = {isSidebarOpen}
+        onClose={()=> setIsSidebarOpen(false)}
+        variant="persistent"
+        anchor="left"
+        sx={{
+          width: drawerWidth,
+          // custom CSS for MUI component look up the CSS class on the page if necessary
+          "& .MuiDrawer-paper": {
+            color: theme.palette.secondary[200],
+            backgroundColor: theme.palette.background.alt,
+            boxSizing: "border-box",
+            borderWidth: isNonMobile ? 0 : "2px",
+            width: drawerWidth
+          }
+        }}
+        >
+          <Box width="100%">
+            <Box m="1.5rem 2rem 2rem 3rem">
+              <FlexBetween color={theme.palette.secondary.main}>
+                <Box display="flex" alignItems="center" gap="0/5rem">
+                  <Typography variant="h4" fontWeight="bold">
+                    ECOMVISION
+                  </Typography>
+                </Box>
+                {!isNonMobile && (
+                  <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                    <ChevronLeft />
+                  </IconButton>
+                )}
+              </FlexBetween>
+            </Box>
+            <List>
 
-export default Sidebar
+            </List>
+          </Box>
+        </Drawer>
+      )}
+    </Box>
+  );
+};
+
+export default Sidebar;
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -6,12 +6,23 @@ import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
 
 const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   return (
-    <Box width="100%" height="100%">
+    <Box display={ isNonMobile ? "flex" : "block" } width="100%" height="100%">
+      <Sidebar
+        isNonMobile={ isNonMobile }
+        drawerWidth='250px'
+        isSidebarOpen={ isSidebarOpen }
+        setIsSidebarOpen={ setIsSidebarOpen }
+      />
       <Box>
         {/* Navbar will exist on each page and the Outlet will contain what is underneath */ }
         {/* from reactrouter.com: An <Outlet> should be used in parent route elements to render their child route elements. This allows nested UI to show up when child routes are rendered. If the parent route matched exactly, it will render a child index route or nothing if there is no index route. */ }
-        <Navbar />
+        <Navbar
+          isSidebarOpen={ isSidebarOpen }
+          setIsSidebarOpen={ setIsSidebarOpen }
+        />
         <Outlet />
       </Box>
     </Box>
