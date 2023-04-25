e9abc65324c4fc0e9a93532c7ff0395d266964c7
Mike Johnson <mike@mapleprintservices.com>
2023-04-24 21:48:58 -0400

    icons on sidebar and sidebar opens and closes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,25 +24,28 @@ import {
   useTheme
 } from '@mui/material';
 
-const Navbar = () => {
+const Navbar = ({
+  isSidebarOpen,
+  setIsSidebarOpen,
+}) => {
   const dispatch = useDispatch();
   const theme = useTheme();
   return (
     <AppBar
-      sx={{
+      sx={ {
         position: "static",
         background: "none",
         boxShadow: "none",
-      }}
+      } }
     >
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        {/* Left side */}
+      <Toolbar sx={ { justifyContent: "space-between" } }>
+        {/* Left side */ }
         <FlexBetween>
-          <IconButton onClick={() => console.log('open/close sidebar')}>
+          <IconButton onClick={ () => setIsSidebarOpen(!isSidebarOpen) }>
             <MenuIcon />
           </IconButton>
           <FlexBetween
-            backgroundColor={theme.palette.background.alt}
+            backgroundColor={ theme.palette.background.alt }
             borderRadius="9px"
             gap="3rem"
             p="0.1rem 1.5rem"
@@ -54,22 +57,22 @@ const Navbar = () => {
           </FlexBetween>
         </FlexBetween>
 
-        {/* Right Side */}
+        {/* Right Side */ }
         <FlexBetween gap="1.5rem">
-          <IconButton onClick={() => dispatch(setMode())}>
-            {theme.palette.mode === 'dark' ? (
-              <DarkModeOutlined sx={{ fontSize: '25px' }} />
-            ) : (<LightModeOutlined sx={{ fontSize: '25px' }} />
-            )}
+          <IconButton onClick={ () => dispatch(setMode()) }>
+            { theme.palette.mode === 'dark' ? (
+              <DarkModeOutlined sx={ { fontSize: '25px' } } />
+            ) : (<LightModeOutlined sx={ { fontSize: '25px' } } />
+            ) }
           </IconButton>
           <IconButton>
-            <SettingsOutlined sx={{ fontSize: '25px' }} />
+            <SettingsOutlined sx={ { fontSize: '25px' } } />
           </IconButton>
         </FlexBetween>
       </Toolbar>
 
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -34,7 +34,7 @@ import {
   PieChartOutlined
 } from "@mui/icons-material";
 
-const navItems =[
+const navItems = [
   {
     text: "Dashboard",
     icon: <HomeOutlined />
@@ -91,7 +91,7 @@ const navItems =[
     text: "Performance",
     icon: <TrendingUpOutlined />
   },
-]
+];
 
 const Sidebar = ({
   drawerWidth,
@@ -113,45 +113,90 @@ const Sidebar = ({
   return (
     // going to use material ui persistent drawer
     <Box component="nav">
-      {isSidebarOpen && (
-        <Drawer 
-        open = {isSidebarOpen}
-        onClose={()=> setIsSidebarOpen(false)}
-        variant="persistent"
-        anchor="left"
-        sx={{
-          width: drawerWidth,
-          // custom CSS for MUI component look up the CSS class on the page if necessary
-          "& .MuiDrawer-paper": {
-            color: theme.palette.secondary[200],
-            backgroundColor: theme.palette.background.alt,
-            boxSizing: "border-box",
-            borderWidth: isNonMobile ? 0 : "2px",
-            width: drawerWidth
-          }
-        }}
+      { isSidebarOpen && (
+        <Drawer
+          open={ isSidebarOpen }
+          onClose={ () => setIsSidebarOpen(false) }
+          variant="persistent"
+          anchor="left"
+          sx={ {
+            width: drawerWidth,
+            // custom CSS for MUI component look up the CSS class on the page if necessary
+            "& .MuiDrawer-paper": {
+              color: theme.palette.secondary[200],
+              backgroundColor: theme.palette.background.alt,
+              boxSizing: "border-box",
+              borderWidth: isNonMobile ? 0 : "2px",
+              width: drawerWidth
+            }
+          } }
         >
           <Box width="100%">
             <Box m="1.5rem 2rem 2rem 3rem">
-              <FlexBetween color={theme.palette.secondary.main}>
+              <FlexBetween color={ theme.palette.secondary.main }>
                 <Box display="flex" alignItems="center" gap="0/5rem">
                   <Typography variant="h4" fontWeight="bold">
                     ECOMVISION
                   </Typography>
                 </Box>
-                {!isNonMobile && (
-                  <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                { !isNonMobile && (
+                  <IconButton onClick={ () => setIsSidebarOpen(!isSidebarOpen) }>
                     <ChevronLeft />
                   </IconButton>
-                )}
+                ) }
               </FlexBetween>
             </Box>
             <List>
+              { navItems.map(({ text, icon }) => {
+                if (!icon) {
+                  return (
+                    <Typography key={ text } sx={ { m: "2.25rem 0 1rem 3rem" } }>
+                      { text }
+                    </Typography>
+                  );
+                }
+                // clicking on the items below navigates to the title of that item in the URL and sets that button to active
+                const lcText = text.toLowerCase();
+                return (
+                  <ListItem key={ text } disablePadding>
+                    <ListItemButton
+                      onClick={ () => {
+                        navigate(`/${lcText}`);
+                        setActive(lcText);
+                      } }
+                      sx={ {
+                        backgroundColor: active === lcText
+                          ? theme.palette.secondary[300]
+                          : "transparent",
+                        color: active === lcText
+                          ? theme.palette.primary[600]
+                          : theme.palette.secondary[100],
+                      } }
+                    >
+                      <ListItemIcon
+                        sx={ {
+                          ml: "2rem",
+                          color: active === lcText
+                            ? theme.palette.primary[600]
+                            : theme.palette.secondary[200],
+                        } }
+                      >
+                        { icon }
+                      </ListItemIcon>
+                      <ListItemText primary={ text } />
+                      { active === lcText && (
+                        <ChevronRightOutlined sx={ { ml: "auto" } } />
+                      ) }
+                    </ListItemButton>
+                  </ListItem>
+                );
+              }
+              ) }
 
             </List>
           </Box>
         </Drawer>
-      )}
+      ) }
     </Box>
   );
 };
